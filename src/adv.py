fff741e551e909a986b4ea1b68b3c50632716a8a
jose-marquez89 <jlmarquez89@gmail.com>
2020-06-03 18:01:17 -0500

    continues building on parser with new class structures

--- src/adv.py
+++ src/adv.py
@@ -1,41 +1,42 @@
 import time
+import textwrap
 
-
-# from room import Room
+from room import Room
 # from player import Player
 
 # Declare all the rooms
 
-# room = {
-#     'outside':  Room("Outside Cave Entrance",
-#                      "North of you, the cave mount beckons"),
-#
-#     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-# passages run north and east."""),
-#
-#     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-# into the darkness. Ahead to the north, a light flickers in
-# the distance, but there is no way across the chasm."""),
-#
-#     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-# to north. The smell of gold permeates the air."""),
-#
-#     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-# chamber! Sadly, it has already been completely emptied by
-# earlier adventurers. The only exit is to the south."""),
-# }
+room = {
+    'outside':  Room("Outside Cave Entrance",
+                     "North of you, the cave mount beckons"),
+
+    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+passages run north and east."""),
+
+    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+into the darkness. Ahead to the north, a light flickers in
+the distance, but there is no way across the chasm."""),
+
+    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+to north. The smell of gold permeates the air."""),
+
+    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+chamber! Sadly, it has already been completely emptied by
+earlier adventurers. The only exit is to the south."""),
+}
 
 
 # Link rooms together
 
-# room['outside'].n_to = room['foyer']
-# room['foyer'].s_to = room['outside']
-# room['foyer'].n_to = room['overlook']
-# room['foyer'].e_to = room['narrow']
-# room['overlook'].s_to = room['foyer']
-# room['narrow'].w_to = room['foyer']
-# room['narrow'].n_to = room['treasure']
-# room['treasure'].s_to = room['narrow']
+room['outside'].n_to = room['foyer']
+room['foyer'].s_to = room['outside']
+room['foyer'].n_to = room['overlook']
+room['foyer'].e_to = room['narrow']
+room['overlook'].s_to = room['foyer']
+room['narrow'].w_to = room['foyer']
+room['narrow'].n_to = room['treasure']
+room['treasure'].s_to = room['narrow']
+
 
 def main():
     greetings = ["Welcome to Generic Adventure 1.0!",
@@ -48,6 +49,11 @@ def main():
 
         time.sleep(1)
 
+    def no_room_error(r):
+        """Keeps current room from becoming None"""
+        if r is None:
+            raise TypeError("Current room cannot be NoneType")
+
     for g in greetings:
         scary_print(g)
         print('\r', end='')
@@ -55,12 +61,53 @@ def main():
     player_name: str = input("Please enter your name: ")
 
     # player_1 = Player(player_name, room["outside"])
-    next_move = None
+    next_move = ''
+    current_room = room['outside']
 
-    while next_move != "q":
-        print("You are currently in: Room")
-        print("This is the description of the Room")
-        next_move = input("Where to next?")
+
+
+    while next_move.lower() != "q":
+        print(f"\r\n{player_name} is currently in {current_room.name}:")
+
+        # print the long description with wrapping
+        for line in textwrap.wrap(f"{current_room.description}"):
+            print('    |', line)
+        next_move = input("Where to next?\nEnter a cardinal direction: ")
+        no_go_message = "Oops, can't go there!"
+        if next_move.lower() == "n":
+            try:
+                new_room = current_room.n_to
+                no_room_error(new_room)
+            except TypeError:
+                print(no_go_message)
+                continue
+        elif next_move.lower() == "e":
+            try:
+                new_room = current_room.e_to
+                no_room_error(new_room)
+            except TypeError:
+                print(no_go_message)
+                continue
+        elif next_move.lower() == "s":
+            try:
+                new_room = current_room.s_to
+                no_room_error(new_room)
+            except TypeError:
+                print(no_go_message)
+                continue
+        elif next_move.lower() == "w":
+            try:
+                new_room = current_room.w_to
+                no_room_error(new_room)
+            except TypeError:
+                print(no_go_message)
+                continue
+        elif next_move.lower() == "q":
+            print("Looks like your quest is over...")
+            continue
+        print(f"Moving To {new_room.name}...", end='')
+        time.sleep(.60)
+        current_room = new_room
 
     print("Thanks for playing!")
 
