086d404422e07d6f8bfa3dfe1678d08fb34363a6
jose-marquez89 <jlmarquez89@gmail.com>
2020-06-04 16:00:53 -0500

    adds a 'show_inventory' method

--- src/player.py
+++ src/player.py
@@ -11,10 +11,17 @@ class Player:
     def grab(self, item: object):
         """Take an item, triggers item 'on_take' printout"""
         self.inventory.append(item)
+        self.location.remove_items(item)
         item.on_grab()
 
     def drop(self, item: object):
         """Drop an item, triggers item 'on_drop' printout"""
         idx = self.inventory.index(item)
+        self.location.add_items(item)
         del self.inventory[idx]
         item.on_drop()
+
+    def show_inventory(self):
+        print("Your Items:")
+        for idx, item in enumerate(self.inventory):
+            print("\t", f"Item no. {idx}: {item}")
