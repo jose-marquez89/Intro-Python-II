93d2135fc51c1d37bd8884208b7b51bd8bfafdc3
jose-marquez89 <jlmarquez89@gmail.com>
2020-06-04 11:44:37 -0500

    adds item functions and attributes

--- src/room.py
+++ src/room.py
@@ -7,3 +7,20 @@ class Room:
         self.e_to = None
         self.s_to = None
         self.w_to = None
+        self.items = []
+
+    def add_items(self, *args):
+        for item in args:
+            self.items.append(item)
+
+    def remove_items(self, *args):
+        for item in args:
+            idx = self.items.index(item)
+            del self.items[idx]
+
+    def __str__(self):
+        return "roomName: %s\nroomDescription: %s" % (self.name,
+                                                      self.description)
+
+    def __repr__(self):
+        return "Room(%s, %s)" % (self.name, self.description)
