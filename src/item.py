dea96034bc9294b487c70e460dad26f85cfacdb8
jose-marquez89 <jlmarquez89@gmail.com>
2020-06-04 12:50:45 -0500

    adds grab and drop methods to item class

--- src/item.py
+++ src/item.py
@@ -3,3 +3,9 @@ class Item:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+
+    def on_grab(self):
+        print(f"You picked up {self.name}!")
+
+    def on_drop(self):
+        print(f"You've dropped {self.name}! It's no longer in your inventory")
