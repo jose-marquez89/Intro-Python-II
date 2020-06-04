38ba2ccae2b7be0c014799714dbcdb59dc038305
jose-marquez89 <jlmarquez89@gmail.com>
2020-06-04 12:51:10 -0500

    adds grab and drop methods to player class

--- src/player.py
+++ src/player.py
@@ -3,6 +3,18 @@ class Player:
     def __init__(self, name, location):
         self.name = name
         self.location = location
+        self.inventory = []
 
     def update_location(self, new_location):
         self.location = new_location
+
+    def grab(self, item: object):
+        """Take an item, triggers item 'on_take' printout"""
+        self.inventory.append(item)
+        item.on_grab()
+
+    def drop(self, item: object):
+        """Drop an item, triggers item 'on_drop' printout"""
+        idx = self.inventory.index(item)
+        del self.inventory[idx]
+        item.on_drop()
